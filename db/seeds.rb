f01152e97f67782514de3db49a2fd05158c23f9d
Tyler Cooper <tyler.cooper91@gmail.com>
2016-08-25 18:10:42 -0400

    unique post and comment created

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,27 @@ require 'random_data'
 # Create Posts
 50.times do
 # #1
-Post.create!(
+  Post.create!(
 # #2
-title:  RandomData.random_sentence,
-body:   RandomData.random_paragraph
-)
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
 end
 posts = Post.all
 
 # Create Comments
 # #3
 100.times do
-Comment.create!(
-# #4
-post: posts.sample,
-body: RandomData.random_paragraph
-)
+  Comment.create!(
+    # #4
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
 end
+
+
+asmt_post = Post.find_or_create_by(title: "title for assignment", body: "body for assignment")
+asmt_comment = Comment.find_or_create_by(post: asmt_post, body: "comment body")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
