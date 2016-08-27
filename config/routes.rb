e9d2982dbb1bbe4e0f0354fcafa6a04586dbb033
Tyler Cooper <tyler.cooper91@gmail.com>
2016-08-27 11:32:42 -0400

    Posts created, listed, routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
+  get 'contact' => 'welcome#contact'
 
-  get 'welcome/faq'
+  # get 'welcome/contact'
+
+  # get 'welcome/faq'
+
+  get 'faq' => 'welcome#faq'
 
   root 'welcome#index'
 
