b1f26550bb3b52469a18fbf2aad11ee6223128bb
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-01 15:21:37 -0400

    code cleanup

--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -75,41 +75,4 @@ RSpec.describe SponsoredPostsController, type: :controller do
       expect(sponsored_post_instance.body).to eq my_sponsored_post.body
     end
   end
-
-  # describe "PUT update" do
-  #   it "updates sponsored_post with expected attributes" do
-  #     new_title = RandomData.random_sentence
-  #     new_body = RandomData.random_paragraph
-  #     new_price = 99
-  #
-  #     put :update, topic_id: my_topic.id, id: my_sponsored_post.id, sponsored_post: {title: new_title, body: new_body, price: 99}
-  #
-  #     updated_sponsored_post = assigns(:sponsored_post)
-  #     expect(updated_sponsored_post.id).to eq my_sponsored_post.id
-  #     expect(updated_sponsored_post.title).to eq new_title
-  #     expect(updated_sponsored_post.body).to eq new_body
-  #   end
-  #
-  #   it "redirects to the updated sponsored_post" do
-  #     new_title = RandomData.random_sentence
-  #     new_body = RandomData.random_paragraph
-  #     new_price = 99
-  #
-  #     put :update, topic_id: my_topic.id, id: my_sponsored_post.id, sponsored_post: {title: new_title, body: new_body, price: 99}
-  #     expect(response).to redirect_to [my_topic, my_sponsored_post]
-  #   end
-  # end
-  #
-  # describe "DELETE destroy" do
-  #   it "deletes the sponsored_post" do
-  #     delete :destroy, topic_id: my_topic.id, id: my_sponsored_post.id
-  #     count = SponsoredPost.where({id: my_sponsored_post.id}).size
-  #     expect(count).to eq 0
-  #   end
-  #
-  #   it "redirects to topic show" do
-  #     delete :destroy, topic_id: my_topic.id, id: my_sponsored_post.id
-  #     expect(response).to redirect_to my_topic
-  #   end
-  # end
 end
