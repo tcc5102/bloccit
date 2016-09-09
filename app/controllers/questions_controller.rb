39cc6ab082481b88109c2080ef8871d7e298cdb3
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-09 11:40:35 -0400

    additional scopes. fixed question resolved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -35,6 +35,7 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @question.title = params[:question][:title]
     @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
 
     if @question.save
       flash[:notice] = "Question was updated."
