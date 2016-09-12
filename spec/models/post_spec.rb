1724892a024c35b9f5a2ff7e7a5ebc09e3a6e2e1
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-12 14:16:17 -0400

    added comments shallow nested into posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,16 +12,15 @@ RSpec.describe Post, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: title, body: body, user: user) }
 
+  it { is_expected.to have_many(:comments) }
 
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
-
 
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_presence_of(:topic) }
   it { is_expected.to validate_presence_of(:user) }
-
 
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
