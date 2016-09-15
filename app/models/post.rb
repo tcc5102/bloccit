1b1eaeafe5ff20adfbb035175c063dba13af20a2
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-15 17:07:48 -0400

    auto favorite own new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
 
   after_create :create_vote
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
   scope :ordered_by_title, -> { order('title DESC') }
@@ -32,6 +33,11 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 
   private
