ca490519af2d8db3a03ee50c0df86eee90699b35
Tyler Cooper <tyler.cooper91@gmail.com>
2016-09-17 10:37:40 -0400

    added factory to test application

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
-  let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
-  let(:name) { RandomData.random_sentence }
-  let(:description) { RandomData.random_paragraph }
-  let(:title) { RandomData.random_sentence }
-  let(:body) { RandomData.random_paragraph }
-  let(:topic) { Topic.create!(name: name, description: description) }
+  # let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+  # let(:name) { RandomData.random_sentence }
+  # let(:description) { RandomData.random_paragraph }
+  # let(:title) { RandomData.random_sentence }
+  # let(:body) { RandomData.random_paragraph }
+  let(:topic) { create(:topic) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
 
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+  # it { is_expected.to have_many(:labelings) }
 
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:votes) }
@@ -29,7 +30,7 @@ RSpec.describe Post, type: :model do
 
   describe "attributes" do
     it "has a title, body, and user attribute" do
-      expect(post).to have_attributes(title: title, body: body, user: user)
+      expect(post).to have_attributes(title: post.title, body: post.body)
     end
   end
 
